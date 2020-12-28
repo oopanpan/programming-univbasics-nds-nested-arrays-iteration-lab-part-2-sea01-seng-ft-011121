6590e805773bcc2aced26a0fefbd12953d911911
Kai Peng Li <oopanpan@gmail.com>
2020-12-28 21:40:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ def find_min_in_nested_arrays(src)
   output_array = []
   temp_array = []
   src.length.times { |i|
-    temp_array[i] = (src[i].sort)
-    output_array.push(temp_array[i][0])
+    temp_array = (src[i].sort)
+    output_array.push(temp_array[0])
   }
   output_array
   
